7e604e7e680a50aac0025430d19551c1a945d1f7
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2020-03-08 16:52:06 -0500

    implements increment

--- src/app/entries/entry.service.ts
+++ src/app/entries/entry.service.ts
@@ -5,11 +5,10 @@ import { Observable, of, throwError } from 'rxjs';
 import { catchError, tap, map } from 'rxjs/operators';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 //import firebase from 'firebase-admin';
-import 'firebase/firestore';
-
+// import 'firebase/firestore';
+import * as firebase from 'firebase';
 
 import { Entry, Seeds, EntryFire } from './entry';
-
 
 @Injectable({
   providedIn: 'root'
@@ -19,86 +18,88 @@ export class EntryService {
   private jsonUrl = 'assets';
   items: Observable<any[]>;
   increment: any;
-    
+
   constructor(
     private http: HttpClient,
     private httpjson: HttpClient,
     private afs: AngularFirestore
-    ) {
+  ) {
+    //console.log(firebase.firestore)
+    this.increment = firebase.firestore.FieldValue.increment(1);
+  }
 
-          //console.log(firebase.firestore)
-          //this.increment = firebase.firestore.FieldValue.increment(1);
-
-     }
-
-getSeeds(id: number): Observable<Seeds> {
-  const sUrl = `${this.jsonUrl}/wrestlers-seeds.json`;
-  console.log(sUrl);
-  return this.httpjson.get<Seeds>(sUrl)
-    .pipe(
+  getSeeds(id: number): Observable<Seeds> {
+    const sUrl = `${this.jsonUrl}/wrestlers-seeds.json`;
+    console.log(sUrl);
+    return this.httpjson.get<Seeds>(sUrl).pipe(
       tap(data => console.log('Seeds: ' + JSON.stringify(data))),
       catchError(this.handleError)
     );
-};
+  }
 
+  /************************************************
+   *
+   * Firestore functions
+   * - replaced the functions with same name without "Fire"
+   ************************************************/
 
-/************************************************
- * 
- * Firestore functions
- * - replaced the functions with same name without "Fire"
- ************************************************/
-
-  getFireEntries(poolsId): Observable<any[]>{
-  return this.afs.collection('picks', ref =>
-  ref.where('poolsId', '==', poolsId)).valueChanges();
-  };
+  getFireEntries(poolsId): Observable<any[]> {
+    return this.afs.collection('picks', ref => ref.where('poolsId', '==', poolsId)).valueChanges();
+  }
 
   getFireEntry(poolsId: number, id: number): Observable<any> {
     //console.log('In Firestore Entry');
-    return this.afs.collection('picks', ref =>
-    ref.where('poolsId', '==', poolsId).where('id', '==', id)).valueChanges();
+    return this.afs
+      .collection('picks', ref => ref.where('poolsId', '==', poolsId).where('id', '==', id))
+      .valueChanges();
   }
 
   getFireIndex(poolsId: number): Observable<any> {
     //console.log('In Firestore Entry');
-    return this.afs.collection('numOfEntries', ref =>
-    ref.where('poolsId', '==', poolsId)).valueChanges();
+    return this.afs
+      .collection('numOfEntries', ref => ref.where('poolsId', '==', poolsId))
+      .valueChanges();
   }
 
-  updateFireIndex(id: string){
-    this.afs.collection("numOfEntries").doc("y1S2XNMMwqh8vOOMrkgW")
-    .update({index: 5});
-    console.log("FireUp +1");
+  updateFireIndex(id: string) {
+    this.afs
+      .collection('numOfEntries')
+      .doc('y1S2XNMMwqh8vOOMrkgW')
+      .update({ index: this.increment });
+    console.log('FireUp +1');
   }
 
   createFireEntry(entry: Entry, poolsId: number, index) {
     // Still cannot get a value for for firebase index correctly???
-    this.afs.collection('picks').add({
-      entryName: entry.entryName,
-      entryUsername: entry.entryUsername,
-      entryPicks: [entry.entryPick1, 
-                      entry.entryPick2,
-                      entry.entryPick3,
-                      entry.entryPick4,
-                      entry.entryPick5,
-                      entry.entryPick6,
-                      entry.entryPick7,
-                      entry.entryPick8,
-                      entry.entryPick9, 
-                      entry.entryPick10,
-                      entry.entryPick11, 
-                      entry.entryPick12,
-                      entry.entryPick13, 
-                      entry.entryPick14,
-                      entry.entryPick15,
-                      entry.entryPick16  
-                      ],
-      id:  index, //NEED THIS TO INCREMENT - cannot read database numOfEntries??
-      poolsId: poolsId
-    })
-    .catch(function(error) {
-      console.error("Error adding document: ", error);
-    });
+    this.afs
+      .collection('picks')
+      .add({
+        entryName: entry.entryName,
+        entryUsername: entry.entryUsername,
+        entryPicks: [
+          entry.entryPick1,
+          entry.entryPick2,
+          entry.entryPick3,
+          entry.entryPick4,
+          entry.entryPick5,
+          entry.entryPick6,
+          entry.entryPick7,
+          entry.entryPick8,
+          entry.entryPick9,
+          entry.entryPick10,
+          entry.entryPick11,
+          entry.entryPick12,
+          entry.entryPick13,
+          entry.entryPick14,
+          entry.entryPick15,
+          entry.entryPick16
+        ],
+        id: index, //NEED THIS TO INCREMENT - cannot read database numOfEntries??
+        poolsId: poolsId
+      })
+      .catch(function(error) {
+        console.error('Error adding document: ', error);
+      });
 
     // Need to just increment index??? .update???
 
@@ -107,39 +108,29 @@ getSeeds(id: number): Observable<Seeds> {
     //     poolsId: indexing[0].poolsId,
     //     index: indexing[0].index + 1
     // });
+  }
 
-    }
-
-
-
-//--------End Firestore functions-----------------
-
-
-
+  //--------End Firestore functions-----------------
 
   // This getEntry(id) & getEntryNames() uses the in-memory-api module for testing.
 
   getEntries(): Observable<Entry[]> {
-    return this.http.get<Entry[]>(this.entriesUrl)
-      .pipe(
-        tap(data => console.log(JSON.stringify(data))),
-        tap(data => console.log(data.map(obj=>obj.entryName)),
-        
-        catchError(this.handleError)
-        )
-      );
-  };
+    return this.http.get<Entry[]>(this.entriesUrl).pipe(
+      tap(data => console.log(JSON.stringify(data))),
+      tap(
+        data => console.log(data.map(obj => obj.entryName)),
 
-
-  getEntryNames(): Observable<Entry[]> {
-    return this.http.get<any[]>(this.entriesUrl)
-      .pipe(
-        map(data=>data.map(obj=>obj.entryName)),
         catchError(this.handleError)
-        );
-      
+      )
+    );
   }
 
+  getEntryNames(): Observable<Entry[]> {
+    return this.http.get<any[]>(this.entriesUrl).pipe(
+      map(data => data.map(obj => obj.entryName)),
+      catchError(this.handleError)
+    );
+  }
 
   getEntry(id: number): Observable<Entry> {
     console.log('In Entry');
@@ -149,17 +140,17 @@ getSeeds(id: number): Observable<Seeds> {
     }
     const url = `${this.entriesUrl}/${id}`;
     console.log(url);
-    return this.http.get<Entry>(url)
-      .pipe(
-        tap(data => console.log('getEntry: ' + JSON.stringify(data))),
-        catchError(this.handleError)
-      );
+    return this.http.get<Entry>(url).pipe(
+      tap(data => console.log('getEntry: ' + JSON.stringify(data))),
+      catchError(this.handleError)
+    );
   }
 
   createEntry(entry: Entry): Observable<Entry> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    entry.id = null;  //DOES NOT HAVE A ID???????????
-    return this.http.post<Entry>(this.entriesUrl, entry, { headers })
+    entry.id = null; //DOES NOT HAVE A ID???????????
+    return this.http
+      .post<Entry>(this.entriesUrl, entry, { headers })
       .pipe(
         tap(data => console.log('createEntry: ' + JSON.stringify(data))),
         catchError(this.handleError)
@@ -169,7 +160,8 @@ getSeeds(id: number): Observable<Seeds> {
   deleteEntry(id: number): Observable<{}> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     const url = `${this.entriesUrl}/${id}`;
-    return this.http.delete<Entry>(url, { headers })
+    return this.http
+      .delete<Entry>(url, { headers })
       .pipe(
         tap(data => console.log('deleteEntry: ' + id)),
         catchError(this.handleError)
@@ -179,7 +171,8 @@ getSeeds(id: number): Observable<Seeds> {
   updateEntry(entry: Entry): Observable<Entry> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     const url = `${this.entriesUrl}/${entry.id}`;
-    return this.http.put<Entry>(url, entry, { headers })
+    return this.http
+      .put<Entry>(url, entry, { headers })
       .pipe(
         tap(() => console.log('updateEntry: ' + entry.id)),
         // Return the entry on an update
@@ -207,27 +200,26 @@ getSeeds(id: number): Observable<Seeds> {
   private initializeEntry(): Entry {
     // Return an initialized object
     return {
-        id: 0,
-        entryUsername: '',
-        entryName: '',
-        entryPick1: '',
-        entryPick2: '',
-        entryPick3: '',
-        entryPick4: '',
-        entryPick5: '',
-        entryPick6: '',
-        entryPick7: '',
-        entryPick8: '',
-        entryPick9: '',
-        entryPick10: '',
-        entryPick11: '',
-        entryPick12: '',
-        entryPick13: '',
-        entryPick14: '',
-        entryPick15: '',
-        entryPick16: '',
-        entryPoints: 0
+      id: 0,
+      entryUsername: '',
+      entryName: '',
+      entryPick1: '',
+      entryPick2: '',
+      entryPick3: '',
+      entryPick4: '',
+      entryPick5: '',
+      entryPick6: '',
+      entryPick7: '',
+      entryPick8: '',
+      entryPick9: '',
+      entryPick10: '',
+      entryPick11: '',
+      entryPick12: '',
+      entryPick13: '',
+      entryPick14: '',
+      entryPick15: '',
+      entryPick16: '',
+      entryPoints: 0
     };
   }
-
 }
